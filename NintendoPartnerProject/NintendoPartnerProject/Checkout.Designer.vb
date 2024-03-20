e6c0becc472e1f0287846ef5007912240eb3873c
Bryant <blee879@learner.dvusd.org>
2024-03-20 08:09:06 -0700

    Additional Checkout Changes

--- NintendoPartnerProject/NintendoPartnerProject/Checkout.Designer.vb
+++ NintendoPartnerProject/NintendoPartnerProject/Checkout.Designer.vb
@@ -27,8 +27,8 @@ Partial Class Checkout
         Me.Label2 = New System.Windows.Forms.Label()
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.CheckBox2 = New System.Windows.Forms.CheckBox()
-        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
         Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
         Me.CheckBox5 = New System.Windows.Forms.CheckBox()
         Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -66,9 +66,9 @@ Partial Class Checkout
         Me.CheckBox1.AutoSize = True
         Me.CheckBox1.Location = New System.Drawing.Point(376, 144)
         Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(97, 20)
+        Me.CheckBox1.Size = New System.Drawing.Size(62, 20)
         Me.CheckBox1.TabIndex = 5
-        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.Text = "Used"
         Me.CheckBox1.UseVisualStyleBackColor = True
         '
         'CheckBox2
@@ -76,39 +76,39 @@ Partial Class Checkout
         Me.CheckBox2.AutoSize = True
         Me.CheckBox2.Location = New System.Drawing.Point(479, 144)
         Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(97, 20)
+        Me.CheckBox2.Size = New System.Drawing.Size(129, 20)
         Me.CheckBox2.TabIndex = 6
-        Me.CheckBox2.Text = "CheckBox2"
+        Me.CheckBox2.Text = "Additional Game"
         Me.CheckBox2.UseVisualStyleBackColor = True
-        '
-        'CheckBox3
-        '
-        Me.CheckBox3.AutoSize = True
-        Me.CheckBox3.Location = New System.Drawing.Point(376, 170)
-        Me.CheckBox3.Name = "CheckBox3"
-        Me.CheckBox3.Size = New System.Drawing.Size(97, 20)
-        Me.CheckBox3.TabIndex = 7
-        Me.CheckBox3.Text = "CheckBox3"
-        Me.CheckBox3.UseVisualStyleBackColor = True
         '
         'CheckBox4
         '
         Me.CheckBox4.AutoSize = True
         Me.CheckBox4.Location = New System.Drawing.Point(479, 170)
         Me.CheckBox4.Name = "CheckBox4"
-        Me.CheckBox4.Size = New System.Drawing.Size(97, 20)
-        Me.CheckBox4.TabIndex = 8
-        Me.CheckBox4.Text = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(149, 20)
+        Me.CheckBox4.TabIndex = 7
+        Me.CheckBox4.Text = "Additional Controller"
         Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(376, 170)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(102, 20)
+        Me.CheckBox3.TabIndex = 8
+        Me.CheckBox3.Text = "Refurbished"
+        Me.CheckBox3.UseVisualStyleBackColor = True
         '
         'CheckBox5
         '
         Me.CheckBox5.AutoSize = True
         Me.CheckBox5.Location = New System.Drawing.Point(376, 196)
         Me.CheckBox5.Name = "CheckBox5"
-        Me.CheckBox5.Size = New System.Drawing.Size(97, 20)
+        Me.CheckBox5.Size = New System.Drawing.Size(95, 20)
         Me.CheckBox5.TabIndex = 9
-        Me.CheckBox5.Text = "CheckBox5"
+        Me.CheckBox5.Text = "HDMI Cord"
         Me.CheckBox5.UseVisualStyleBackColor = True
         '
         'Button1
@@ -126,8 +126,8 @@ Partial Class Checkout
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.CheckBox5)
-        Me.Controls.Add(Me.CheckBox4)
         Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox4)
         Me.Controls.Add(Me.CheckBox2)
         Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.Label2)
@@ -146,8 +146,8 @@ Partial Class Checkout
     Friend WithEvents Label2 As Label
     Friend WithEvents CheckBox1 As CheckBox
     Friend WithEvents CheckBox2 As CheckBox
-    Friend WithEvents CheckBox3 As CheckBox
     Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
     Friend WithEvents CheckBox5 As CheckBox
     Friend WithEvents Button1 As Button
 End Class
